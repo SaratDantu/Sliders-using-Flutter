21faea727d89c17ffb0e0eb84eea1ea9e23c5b6f
saratdantu02 <dantusarat@gmail.com>
2022-11-20 15:10:03 +0530

    Sliders

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sliders in Flutter
+
+This repo contains code for **Slider** and **RangeSlider** widgets to help you quickly get started with them.
+
+Checkout the video [here](https://youtu.be/jzK9wLz_0iQ)
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:screen/screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Domodigx",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.orange,
+        body: Center(
+          child: BrightnessSliderContainer(),
+
+        ),
+      ),
+    );
+  }
+}
+
+class BrightnessSliderContainer extends StatefulWidget {
+  @override
+  _BrightnessSliderContainerState createState() => _BrightnessSliderContainerState();
+}
+
+class _BrightnessSliderContainerState extends State<BrightnessSliderContainer> {
+  double brightness = 0.0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return /* Slider  */ CupertinoSlider( 
+        activeColor: CupertinoColors.black,
+        min: 0.0,
+        max: 1.0,
+        value: brightness,
+        onChanged: (val) {
+          Screen.setBrightness(brightness);
+          setState(() {
+            brightness = val;
+          });
+        },
+      );
+  }
+}
+
+
+class RangeSliderWidget extends StatefulWidget {
+  @override
+  _RangeSliderWidgetState createState() => _RangeSliderWidgetState();
+}
+
+class _RangeSliderWidgetState extends State<RangeSliderWidget> {
+   static double _lowerValue = 0.0;
+  static double _upperValue = 10.0;
+
+  RangeValues values = RangeValues(_lowerValue, _upperValue);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliderTheme(
+      data: SliderTheme.of(context).copyWith(
+        trackHeight: 15,
+        overlayColor: Colors.transparent,
+        minThumbSeparation: 100,
+        rangeThumbShape: RoundRangeSliderThumbShape(
+          enabledThumbRadius: 10,
+          disabledThumbRadius: 10,
+        ),
+      ),
+      child: RangeSlider( 
+        activeColor: CupertinoColors.activeGreen,
+        labels: RangeLabels(values.start.abs().toString(), values.end.abs().toString()),
+        min: 0.0,
+        max: 1.0,
+        values: values,
+        onChanged: (val) {
+          setState(() {
+            values = val;
+          });
+        },
+      ),
+    );
+  }
+}
